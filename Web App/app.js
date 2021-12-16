87fd3ce06de6c6520d25d58067e31638e375f092
Luka Petrovic <lukapetrovic118@gmail.com>
2021-12-16 16:27:38 +0100

    New client comments

--- Web App/app.js
+++ Web App/app.js
@@ -25,25 +25,32 @@ socket.onopen = (e) => {
 socket.onmessage = (event) => {
 
     msgBox.textContent = "Server synchronization...";
-    let data = new Response(event.data);
 
+    // Get server response as binary data
+    let data = new Response(event.data);
+    // Convert binary to array buffer data
     data.arrayBuffer().then((buffer) => {
         try {
-
-            // 16 Bytes for metadata
-            // 1 - Full frame
-            // 2 - Difference
+            // 4 Bytes for metadata, first byte - package code
+            // Code 0 - Server rror
+            // Code 1 - Sync package
+            // Code 2 - Frame difference package
             let metadataBuffer = buffer.slice(0, 16);
             let metadata = new Int32Array(metadataBuffer);
 
+            // View data as 1 byte elements 
             let byteView = new Uint8Array(buffer);
+
+            // Sort packages
             if (metadata[0] == 1) {
                 insertFullFrame(byteView.slice(16));
                 firstFullFrame = true;
                 msgBox.style = "display: none";
+              // Skip if sync package has not arrived yet
             } else if (metadata[0] == 2 && firstFullFrame == true) {
                 insertCompressedFrame(byteView.slice(16), metadata[1], metadata[2], metadata[3]);
             } else {
+                // If error package, ignore
                 return;
             }
         } catch (error) {
@@ -87,16 +94,15 @@ function insertCompressedFrame(dataBuffer, numRed, numGreen, numBlue) {
     let greenPixelsBuffer = new Array(pixelNum);
     let bluePixelsBuffer = new Array(pixelNum);
 
-
-    // Response data memory view
+    // View data as an array of 1 byte elements
     let dataView = new Uint8Array(dataBuffer);
-
 
     // Decode pixels
     let redCounter = 0;
     let greenCounter = 0;
     let blueCounter = 0;
 
+    // Unpack data using reverse run length encoding
     dataView.map((value, index) => {
         if (index % 2 == 0) {
             if (index < numRed) {
@@ -115,15 +121,17 @@ function insertCompressedFrame(dataBuffer, numRed, numGreen, numBlue) {
         }
     })
 
-    // Image Data - RGBA
+    // Substract the base pixel values from the frame residuals
     for (let i = 0; i < pixelNum; i++) {
         let red = imageData.data[(i * 4) + 0] - redPixelsBuffer[i];
         let green = imageData.data[(i * 4) + 1] - greenPixelsBuffer[i];
         let blue = imageData.data[(i * 4) + 2] - bluePixelsBuffer[i];
+        
         imageData.data[(i * 4) + 0] = red;
         imageData.data[(i * 4) + 1] = green;
         imageData.data[(i * 4) + 2] = blue;
     }
+    // Put the new values on the canvas
     canvas.putImageData(imageData, 0, 0);
 
 }
